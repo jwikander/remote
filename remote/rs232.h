5cce5a7dd090f63dc5f50474a0be996f0c57e73b
Johan Wikander <jwikander@Johan-Wikanders-MacBook-Air.local>
2011-03-12 23:27:07 +0100

    rs232

---
+++ remote/rs232.h
@@ -0,0 +1,16 @@
+//
+//  rs232.h
+//  remote
+//
+//  Created by Johan Wikander on 3/12/11.
+//  Copyright 2011 None. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface rs232 : NSObject {
+    
+}
+
+@end
