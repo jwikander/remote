95834d61fffaf682dcdfe339b63f421395f68998
jwikander <jwikander@gmail.com>
2011-03-15 19:14:43 +0100

    Implementing part of the port functions

--- remote/rs232.h
+++ remote/rs232.h
@@ -2,18 +2,24 @@
 //  rs232.h
 //  remote
 //
-//  Created by Johan Wikander on 3/12/11.
+//  Created by JW on 3/12/11.
 //  Copyright 2011 None. All rights reserved.
 //
 
+#import <sys/ioctl.h>
 #import <stdio.h>
+#import <termios.h>
 
 #import <Foundation/Foundation.h>
 
 
 @interface rs232 : NSObject {
     int fileDescriptor;
+    struct termios optionsOriginal;
+    struct termios optionsNew;
 }
+
+- (id)init;
 - (void)openPort: (NSString*) bsdPath;
 - (void)closePort;
     
